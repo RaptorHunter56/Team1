5749cbf80b0c6f4426d505a5c5e6706eca74cc29
steven brown <sb.nb.lego@Gmail.com>
2018-07-08 18:57:48 +1200

    SB - Theme Start

---
+++ Language Swopper/Language Swopper/Theme.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Language_Swopper
+{
+    class Theme
+    {
+        public static string Name { get; }
+
+
+        public static string MainColor { get; }
+        public static string TextColor { get; }
+        public static string FrameColor { get; }
+        public static string ToolBarColor { get; }
+        public static string LinkColor { get; }
+        public static string SectionColor { get; }
+        public static string backgroundColor { get; }
+        
+
+
+        public Theme(string[8] properties)
+        {
+
+        }
+        
+    }
+}
